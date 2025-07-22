a7fa3f3ba4b3d1fbdce91d644de5e6f899ae6ab6
Ertuğrul Erdem Küçük <161616508+ErtugrulErdem@users.noreply.github.com>
2025-07-22 21:06:33 +0300

    first commit for python

---
+++ degiskenler.ornek.py
@@ -0,0 +1,19 @@
+
+#Bir ögrencinin kullanıcı bilgilerini ve mezuniyet notunu tanımlayan bir örneklendirme yaz
+
+studentName = "Ertugrul"
+studentSurname = "Kucuk"
+student = studentName + studentSurname
+studentGender = "male"
+studentID = "123456789"
+studentBirthYear = 2002
+studentAdress = "Kartal/İstanbul"
+studentAge = 2023 - studentBirthYear
+
+year1 = 61.45
+year2 = 79.9
+year3 = 86.95
+year4 =87.34
+gradPoint = ((year1 + year2 + year3 + year4) / 4)
+
+print(gradPoint)
