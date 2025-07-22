1e0cde08a9fe91087e93336b8ecf19501277ad85
Ertuğrul Erdem Küçük <161616508+ErtugrulErdem@users.noreply.github.com>
2025-07-22 22:42:01 +0300

    Stting formatları ve cümleyi tersten yazdırma

---
+++ strings.format.py
@@ -0,0 +1,37 @@
+##### FORMAT METODU #####
+
+
+#yolcuadi = "Ertugrul"
+#yolcusoyadi = "Kucuk"
+
+# print("Yolcu adi: {0} {1}".format(yolcuadi , yolcusoyadi))
+# print("Yolcu adi: {1} {1}".format(yolcuadi , yolcusoyadi))
+# print("Yolcu adi: {1} {0}".format(yolcuadi , yolcusoyadi))
+#print("Yolcu adi: {} {}".format(yolcuadi , yolcusoyadi))
+
+
+
+##### F METODU #####
+
+# name = "Ertugrul"
+# surname = "Kucuk"
+
+# print(f"benim adim :{name}{surname}")
+
+# hesap = 2000/300
+# print(f"sonuc: {hesap}")
+
+# isim , soyisim , yas , meslek = "Ertugrul" , "Kucuk" , 23 "Bilgisayar Muhendisi"
+#Benim adım Ertugrul Kucum yasim 23 ve meslegim Bilgisayar Muhendisi yazdır , tersini de yazdır
+
+isim = "Ertugrul"
+soyisim = "Kucuk"
+yas = 23 
+meslek = "Bilgisayar Muhendisi"
+
+print(f"Benim adım {isim} {soyisim} yasim {yas} ve meslegim {meslek}")
+
+cumle = f"Benim adım {isim} {soyisim}, yaşım {yas} ve mesleğim {meslek}"
+print(cumle[::-1])
+
+
