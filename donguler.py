1892e2c880c89cd99971d19a9148fe9d37c752ac
Ertuğrul Erdem Küçük <161616508+ErtugrulErdem@users.noreply.github.com>
2025-08-04 19:42:54 +0300

    Döngüler

---
+++ donguler.py
@@ -0,0 +1,43 @@
+############################FOR DÖNGÜSÜ############################
+# list = [1,2,3,4,5]
+
+# for sayi in list:
+#     print(sayi)
+
+# isim = "ertugrul kucuk"
+ 
+# for harf in isim:
+#     print(harf)
+
+
+############################WHİLE DÖNGÜSÜ############################
+# a = 1
+# while a <= 10 : 
+#     print(a)
+#     a = a + 1   #a += 1
+
+# list = [1,2,3,4,5,6,7]
+
+# a = 0
+# while a < len(list):
+#     print(list[a])
+#     a += 1
+
+# sayi = int(input("Bir sayi giriniz: "))
+
+# x = 0
+# while x < sayi:
+#     print(x)
+#     x += 1
+
+#### Kullanıcıdan 5 sayı al listenin içine at ve ekranda listeyi sıralı şekilde yazdır
+
+list = []
+
+x = 0
+while x < 5:
+    sayi = int(input("bir sayi giriniz: "))
+    list.append(sayi)
+    x += 1
+list.sort()
+print(list)
